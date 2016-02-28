fc1dfa0d42739889437cd7c962ed63b449adc631
ramses <ramses.deco@gmail.com>
2016-02-28 15:47:43 -0600

    adding route R-A122 Asarco - Macroplaza

--- www/templates/home/home.controller.js
+++ www/templates/home/home.controller.js
@@ -6,6 +6,7 @@
 	function HomeCtrl($scope, $state, $interval,BusesService){
 
 		BusesService.getBuses().then(function(data){
+			console.log(data)
 			$scope.buses = data;
 
 
@@ -14,12 +15,30 @@
 			}
 
 			var i = 0;
+			var j = 0;
 			$interval(updatePosition, 3000);
 			function updatePosition(){ 
-				var latlng = new google.maps.LatLng($scope.buses[0].wayPoints[i].lat, $scope.buses[0].wayPoints[i].lng);
-				$scope.map.markers[1].setPosition(latlng);
-				$scope.map.markers[1].setMap($scope.map);
-				i++;
+
+					if(typeof $scope.buses[0].wayPoints[i] != "undefined"){
+						var latlng = new google.maps.LatLng($scope.buses[0].wayPoints[i].lat, $scope.buses[0].wayPoints[i].lng);
+						$scope.map.markers[1].setPosition(latlng);
+						$scope.map.markers[1].setMap($scope.map);
+					}
+					i++;
+					if(i== $scope.buses[0].wayPoints.length){
+						i = 0;
+					}
+
+					if(typeof $scope.buses[1].wayPoints[j] != "undefined"){
+						var latlng = new google.maps.LatLng($scope.buses[1].wayPoints[j].lat, $scope.buses[1].wayPoints[j].lng);
+						$scope.map.markers[2].setPosition(latlng);
+						$scope.map.markers[2].setMap($scope.map);
+					}
+					j++;
+					
+					if(j == $scope.buses[1].wayPoints.length){
+						j = 0;
+					}
 			}
 
 		});
